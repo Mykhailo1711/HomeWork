1da152102eac114ec398e6d648e2cd60a4cfd1f0
Mykhailo1711 <mihailponomarenko5@gmail.com>
2020-05-24 14:08:36 +0200

    Stupid letter counter

--- lesson4/LetterCounterNew.java
+++ lesson4/LetterCounterNew.java
@@ -1,22 +1,19 @@
 public class LetterCounterNew {
+
     public static void main(String[] args) {
         String test = "This is a test text";
         int TCount = test.length() - test.replace("T", "").length();
-        System.out.println("Amount of letter T is: "+TCount);
         int hCount = test.length() - test.replace("h", "").length();
-        System.out.println("Amount of letter h is: "+hCount);
         int iCount = test.length() - test.replace("i", "").length();
-        System.out.println("Amount of letter i is: "+iCount);
         int sCount = test.length() - test.replace("s", "").length();
-        System.out.println("Amount of letter s is: "+sCount);
         int aCount = test.length() - test.replace("a", "").length();
-        System.out.println("Amount of letter a is: "+aCount);
         int tCount = test.length() - test.replace("t", "").length();
-        System.out.println("Amount of letter t is: "+tCount);
         int eCount = test.length() - test.replace("e", "").length();
-        System.out.println("Amount of letter e is: "+eCount);
         int xCount = test.length() - test.replace("x", "").length();
-        System.out.println("Amount of letter x is: "+xCount);
+        System.out.println("Amount of letters are: "+" T - " +TCount+ " , " + "h - " + hCount +" , "
+                +"i - "+iCount+" , "+"s - " +sCount+" , "
+                + "a - "+aCount+" , "+"t - "+tCount+
+                " , "+ "e - "+eCount+" , "+"x - "+xCount) ;
 
 
 
